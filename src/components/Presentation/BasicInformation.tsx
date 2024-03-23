fa00a400a0014e80482cbd040c1da6937a40eca7
jhonog <jospina@vortexbird.com>
2024-03-23 00:09:13 -0500

    Ajustes visuales

--- src/components/Presentation/BasicInformation.tsx
+++ src/components/Presentation/BasicInformation.tsx
@@ -5,10 +5,10 @@ export const BasicInformation = () => {
       <div className='flex w-full justify-center lg:justify-start'>
         <img src="./foto.png" alt="" className='w-2/3' />
       </div>
-      <h1 className='font-bold text-6xl text-principal-0 '>Jhon Ospina</h1>
-      <h2 className='font-semibold text-lg text-principal-0 mt-3'>Front-end developer</h2>
-      <p className='text-principal-50 mt-3 [@media(max-height:824px)]:hidden'>I'm a proactive mid-senior frontend developer, blending solid engineering principles with innovative design to craft seamless digital experiences.</p>
-      <div className='flex gap-2 py-3 '>
+      <h1 className='font-bold text-6xl text-principal-0 text-center lg:text-left'>Jhon Ospina</h1>
+      <h2 className='font-semibold text-2xl lg:text-lg text-principal-0 mt-3 text-center lg:text-left'>Front-end developer</h2>
+      <p className='hidden lg:block text-principal-50 mt-3 text-center lg:text-left [@media(max-height:824px)]:hidden'>I'm a proactive mid-senior frontend developer, blending solid engineering principles with innovative design to craft seamless digital experiences.</p>
+      <div className='flex gap-2 py-3 justify-center lg:justify-start'>
         {
           socialMedia.map(social => (
             <a key={social.id} target='_blank' href={social.link} className='cursor-pointer text-3xl text-principal-50 hover:text-principal-0'>
--- src/components/Presentation/Navigator.tsx
+++ src/components/Presentation/Navigator.tsx
@@ -6,18 +6,20 @@ interface NavigatorProps {
 
 export const Navigator = ({ scrollToAbout, scrollToExperience, scrollToProjects }: NavigatorProps) => {
     return (
-        <div className='w-full mt-10 [@media(max-height:824px)]:mt-6'>
+        <div className='flex flex-col items-center lg:items-start w-full mt-10 [@media(max-height:824px)]:mt-6 just'>
             <div className="flex items-center text-principal-50 py-2 group hover:text-principal-0 cursor-pointer" onClick={scrollToAbout}>
                 <div className="w-5 bg-principal-50 h-1 mr-2 transition-all duration-300 transform group-hover:w-16 group-hover:bg-white" />
                 <p>
                     ABOUT
                 </p>
+                <div className="block lg:hidden w-5 bg-principal-50 h-1 ml-2 transition-all duration-300 transform group-hover:w-16 group-hover:bg-white" />
             </div>
             <div className="flex items-center text-principal-50 py-2 group hover:text-principal-0 cursor-pointer" onClick={scrollToExperience}>
                 <div className="w-5 bg-principal-50 h-1 mr-2 transition-all duration-300 transform group-hover:w-16 group-hover:bg-white" />
                 <p >
                     EXPERIENCE
                 </p>
+                <div className="block lg:hidden w-5 bg-principal-50 h-1 ml-2 transition-all duration-300 transform group-hover:w-16 group-hover:bg-white" />
             </div>
 
             <div className="flex items-center text-principal-50 py-2 group hover:text-principal-0 cursor-pointer" onClick={scrollToProjects}>
@@ -25,6 +27,7 @@ export const Navigator = ({ scrollToAbout, scrollToExperience, scrollToProjects 
                 <p>
                     PROJECTS
                 </p>
+                <div className="block lg:hidden w-5 bg-principal-50 h-1 ml-2 transition-all duration-300 transform group-hover:w-16 group-hover:bg-white" />
             </div>
         </div>
     )
